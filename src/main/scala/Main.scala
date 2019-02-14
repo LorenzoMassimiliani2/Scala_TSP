1f68be1e8676a4857de4347d32181c62b1d75c32
Lorenzo Vainigli <lorenzovngl@gmail.com>
2019-02-14 11:44:06 +0100

    Added function formatTime() to pretty print the time of execution

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,25 @@
 object Main {
 
-  def time[R](block: => R): R = {
+  def time[R](block: => R): Long = {
     val t0 = System.nanoTime()
     val result = block    // call-by-name
     val t1 = System.nanoTime()
-    println("Elapsed time: " + ((t1 - t0)/1000000000).toInt + "s")
-    result
+    //println("Elapsed time: " + ((t1 - t0)/1000000000).toInt + "s")
+    t1 - t0
+  }
+
+  def formatTime(time: Long): String = {
+    val s = (time*scala.math.pow(10, -9)).toInt
+    val ms =  (time*scala.math.pow(10, -6) - s*scala.math.pow(10, 3)).toInt
+    val us =  (time*scala.math.pow(10, -3) - ms*scala.math.pow(10, 3) - s*scala.math.pow(10, 6)).toInt
+    val ns =  (time - us*scala.math.pow(10, 3) - ms*scala.math.pow(10, 6) - s*scala.math.pow(10, 9)).toInt
+    time + "\n" + s + " s, " + ms + " ms, " + us + " us, " + ns + " ns."
   }
 
   def main(args: Array[String]): Unit = {
     val generateData = new GenerateData(1,10)
     val tsp = new TSP_SPARK()
-    time(tsp.main())
+    println(formatTime(time(tsp.main())))
   }
 
 
